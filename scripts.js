71e08ffdaac5c60d918231c5451956da5017f967
Theshan <theshan.puvi@gmail.com>
2022-01-22 17:23:46 -0500

    added some of playRound function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-let computerAnswer
-
 function computerPlay() {
-    const computerChoice = ['Rock', 'Paper','Scissors']
-    computerAnswer = computerChoice[Math.floor(Math.random() * computerChoice.length)]
+    const computerChoice = ['rock', 'paper','scissors']
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)]
 }
 
-computerPlay()
-console.log(computerAnswer)
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'TIE'
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'PLAYER WINS';
+    } else {
+        return 'NO ONE WINS';
+    }
+}
+
+playerSelection = 'rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection));
