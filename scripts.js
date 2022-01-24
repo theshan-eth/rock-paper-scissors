937eedfc2d2f4b3d6a1eec7e8973710a7d85f8b8
Theshan <theshan.puvi@gmail.com>
2022-01-24 15:03:40 -0500

    added game loop, score and player prompt

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 function computerPlay() {
     const computerChoice = ['rock', 'paper','scissors']
     return computerChoice[Math.floor(Math.random() * computerChoice.length)]
@@ -7,13 +10,28 @@ function playRound(playerSelection,computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
         return 'TIE!'
     } else if((playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') || (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') || (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')){
+        playerScore ++;
         return 'PLAYER WINS!';
     } else if((playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') || (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissors') || (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper')){
+        computerScore ++;
         return 'COMPUTER WINS!';
     }
 }
 
-playerSelection = 'sciSSOrs'
-const computerSelection = computerPlay()
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+for (let round = 0; round < 5; round++) {
+    playerSelection = prompt('Please Enter Rock, Paper or Scissors')
+    const computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
+}
+
+console.log(playerScore);
+console.log(computerScore);
+
+if (playerScore > computerScore) {
+    console.log("Congratualtions you won!");
+} else if (playerScore< computerScore) {
+    console.log("Sorry you lost the game!");
+} else {
+    console.log("Sorry you tied the game!")
+}
